39e060e868f431006ec50aed4022a0dc28d25a18
TunedayCodes <92256566+TunedayCodes@users.noreply.github.com>
2022-07-21 21:56:11 +0100

    updated web files
    few changes to the index.html as well as the style.css also the script.js was modified.

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ let possibleValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9","a", "b",
 
 let password1El = document.getElementById("password1-el")
 let password2El = document.getElementById("password2-el")
-let password3El = document.getElementById("password3-el")
-let password4El = document.getElementById("password4-el")
 
 function getRandomValue() {
   let rand = Math.floor(Math.random() * 74)
@@ -23,11 +21,7 @@ function getPassword() {
 function generatePassword() {
   let password1 = getPassword()
   let password2 = getPassword()
-  let password3 = getPassword()
-  let password4 = getPassword()
   
   password1El.textContent = password1
   password2El.textContent = password2
-  password3El.textContent = password3
-  password4El.textContent = password4
 }
